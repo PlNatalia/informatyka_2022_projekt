ca245eb516c715aee65322d2839e8e0bec883c00
PlNatalia <118690081+PlNatalia@users.noreply.github.com>
2022-11-20 21:09:17 +0100

    Initial commit

---
+++ Źródło.cpp
@@ -0,0 +1,12 @@
+/*
+Gra bêdzie w stylu Space invaders, gracz bêdzie sterowa³ obiektem lewo/prawo i strzela³ w nadci¹gaj¹cych przeciwników. 
+Bêdzie kilka poziomów trudnoœci, zwiêkszaj¹cych siê przejœciu ka¿dego z nich. Za ka¿dego trafionego przeciwnika gracz otrzyma punkty.
+Obiekt sterowany przez gracza bêdzie otrzymywa³ ujemne punkty za ka¿dego pominiêtego przeciwnika i traci³ jedno z killku serc. 
+Utracenie wszystkich skutkowaæ bêdzie zakoñczeniem gry. Uzyskane wyniki bêd¹ widoczne po wybraniu opcji z menu gry.
+*/
+
+int main()
+{
+
+	return 0;
+}
